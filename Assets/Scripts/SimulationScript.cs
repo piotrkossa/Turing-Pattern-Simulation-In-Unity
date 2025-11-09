4c544eee2a5f38574909211183ade4f3e1711e72
piotrkossa <135640588+piotrkossa@users.noreply.github.com>
2025-11-09 15:39:35 +0100

    first working prototype

--- Assets/Scripts/SimulationScript.cs
+++ Assets/Scripts/SimulationScript.cs
@@ -3,30 +3,81 @@ using UnityEngine.UI;
 
 public class SimulationScript : MonoBehaviour
 {
+    [Header("Setup")]
     public ComputeShader computeShader;
-    public RenderTexture renderTexture;
 
     public RawImage rawImage;
 
+    public RenderTexture[] buffers;
+    private int currentBuffer = 0;
 
-    private void Update()
+    private int mainKernel;
+    private int initKernel;
+    private int seedKernel;
+
+    [Header("Simulation Parameters")]
+    public int resolution = 512;
+    [Range(0f, 0.1f)] public float feedRate = 0.0545f;
+    [Range(0f, 0.1f)] public float killRate = 0.062f;
+    [Range(0f, 2f)] public float diffusionA = 1.0f;
+    [Range(0f, 2f)] public float diffusionB = 0.5f;
+    [Range(0f, 5f)] public float timeStep = 1.0f;
+
+
+    void Start()
     {
-        if (renderTexture == null)
+        buffers = new RenderTexture[2];
+        for (int i = 0; i < 2; i++)
         {
-            CreateRenderTexture();
+            buffers[i] = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.RGFloat);
+            buffers[i].enableRandomWrite = true;
+            buffers[i].Create();
         }
 
-        computeShader.SetTexture(0, "Result", renderTexture);
-        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+        mainKernel = computeShader.FindKernel("CSMain");
+        initKernel = computeShader.FindKernel("Initialize");
+        seedKernel = computeShader.FindKernel("AddSeed");
+        
+        computeShader.SetInt("resolution", resolution);
+        
+        computeShader.SetTexture(initKernel, "Result", buffers[0]);
+        computeShader.Dispatch(initKernel, resolution / 8, resolution / 8, 1);
+        
+        computeShader.SetTexture(seedKernel, "Result", buffers[0]);
+        computeShader.Dispatch(seedKernel, resolution / 8, resolution / 8, 1);
 
-        rawImage.texture = renderTexture;
+        currentBuffer = 0;
+
+        rawImage.texture = buffers[currentBuffer];
     }
 
 
-    private void CreateRenderTexture()
+    private void FixedUpdate()
     {
-        renderTexture = new RenderTexture(256, 256, 24);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
+        computeShader.SetFloat("feedRate", feedRate);
+        computeShader.SetFloat("killRate", killRate);
+        computeShader.SetFloat("diffusionU", diffusionA);
+        computeShader.SetFloat("diffusionV", diffusionB);
+        computeShader.SetFloat("deltaTime", timeStep);
+
+        computeShader.SetTexture(mainKernel, "prevState", buffers[currentBuffer]);
+        computeShader.SetTexture(mainKernel, "Result", buffers[1 - currentBuffer]);
+
+        computeShader.Dispatch(mainKernel, resolution / 8, resolution / 8, 1);
+
+        currentBuffer = 1 - currentBuffer;
+
+        rawImage.texture = buffers[currentBuffer];
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (buffers != null && buffers[i] != null)
+            {
+                buffers[i].Release();
+            }
+        }
     }
 }
